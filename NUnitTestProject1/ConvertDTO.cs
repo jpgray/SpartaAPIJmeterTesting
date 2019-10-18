b606c865e0a36ec623cae5b5fe34d9bf9b373726
Jonathan Gray <JGray@spartaglobal.com>
2019-10-18 14:01:29 +0100

    Added test for latest rates

---
+++ NUnitTestProject1/ConvertDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject1
+{
+    public class Query
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public int amount { get; set; }
+    }
+
+    public class Info
+    {
+        public int timestamp { get; set; }
+        public double rate { get; set; }
+    }
+
+    public class ConvertDTO
+    {
+        public bool success { get; set; }
+        public Query query { get; set; }
+        public Info info { get; set; }
+        public string date { get; set; }
+        public double result { get; set; }
+    }
+}
--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,6 +1,9 @@
 using FluentAssertions;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using RestSharp;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace NUnitTestProject1
 {
@@ -21,10 +24,43 @@ namespace NUnitTestProject1
         [Test]
         public void LatestHealthCheck()
         {
-            _request.Resource = "latest/{apiKey}";
-            _request.AddUrlSegment("apiKey", FixerAPIKey);
+            _request.Resource = "latest/";
+            _request.AddQueryParameter("access_key", FixerAPIKey);
             var response = _client.Execute(_request);
             response.StatusCode.ToString().Should().Be("OK");
+        }
+
+        private void AddQueryParameters(IList<string> names, IList<string> values)
+        {
+            for(int i = 0; i < names.Count; i++)
+            {
+                _request.AddQueryParameter(names[i], values[i]);
+            }
+        }
+
+        //will fail, subscription does not have access to convert api
+        [Test]
+        [Ignore("Invalid Subscription Plan")]
+        public void ConvertGBPtoJPY()
+        {
+            _request.Resource = "convert";
+            AddQueryParameters(new List<string> { "access_key", "from", "to", "amount" }, new List<string> { FixerAPIKey,"GBP","JPY","25"});
+            var response = _client.Execute<ConvertDTO>(_request);
+            response.Data.success.Should().BeTrue();
+        }
+
+        [Test]
+        public void LatestRatesResponse()
+        {
+            _request.Resource = "latest";
+            _request.AddQueryParameter("access_key", FixerAPIKey);
+            var response = _client.Execute<LatestDTO>(_request);
+            response.Data.success.Should().BeTrue();
+            foreach(var property in response.Data.rates.GetType().GetProperties())
+            {
+                Assert.Greater((double)property.GetValue(response.Data.rates),0.0);
+                double rate = (double)property.GetValue(response.Data.rates);
+            }
         }
     }
 }
